69d8926351d66fc5d91d268e643e7fb15578cd72
vinay kumar <mrkool.vinay@gmail.com>
2020-03-02 23:29:07 +0530

    initial commit

---
+++ src/gcache/Main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+)
+
+// Item is a group of properties
+type Item struct {
+	Id    string
+	Name  string
+	Price float64
+}
+
+var items []Item = []Item{}
+
+func main() {
+	fmt.Println("Welcome to Gcache")
+	r := mux.NewRouter()
+	r.HandleFunc("/", welcomeFunc)
+	r.HandleFunc("/{id}", findCacheByID).Methods("GET")
+	r.HandleFunc("/add", addToCache).Methods("POST")
+
+	http.ListenAndServe(":8081", r)
+}
+
+func welcomeFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to GCache")
+}
+
+func findCacheByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Println(id)
+}
+
+func addToCache(w http.ResponseWriter, r *http.Request) {
+	// Get request data as JSON from JSON body
+	var newItem Item
+	json.NewDecoder(r.Body).Decode(&newItem)
+
+	items = append(items, newItem)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(items)
+}
